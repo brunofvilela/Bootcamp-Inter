86f2819e50ae14314fd49df1924e68058280c21f
Bruno Franco <brunofvilela1996@gmail.com>
2022-01-03 14:42:56 -0300

    Desafios

---
+++ Desafios/desafio5.js
@@ -0,0 +1,16 @@
+let lines = gets().split('\n')
+let line = lines.shift().split(' ')
+
+console.log(line.reduce((ac, item) => Number(ac) * Number(item)))
+
+/*
+Na geometria Euclidiana, um polígono regular é um polígono em que todos os ângulos são iguais e todos os lados tem o mesmo comprimento. Um polígono simples é aquele cujos segmentos de reta não se interceptam. 
+
+Você deve escrever um programa que, dados o número e o comprimento dos lados de um polígono regular, mostre seu perímetro.
+
+Entrada
+A entrada tem dois inteiros positivos: N e L, que são, respectivamente, o número de lados e o comprimento de cada lado de um polígono regular (3 ≤ N ≤ 1000000 and 1 ≤ L ≤ 4000).
+
+Saída
+A saída é o perímetro P do polígono regular em uma única linha.
+ */
