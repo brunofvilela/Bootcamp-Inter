ed71997d026861a207065191906334cc86d51f51
Bruno Franco <brunofvilela1996@gmail.com>
2022-01-18 10:25:13 -0300

    Update db.ts

--- node-user-authentication-api/src/db.ts
+++ node-user-authentication-api/src/db.ts
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
 
-const connectionString = 'postgres://ndtjomvp:2DxCe7iGnPsxg6dTTGg79O8q2xb9aBlB@motty.db.elephantsql.com/ndtjomvp';
+const connectionString = '';
 const db = new Pool({connectionString})
 export default db;
