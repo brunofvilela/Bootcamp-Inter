0d7725293dc7537c98127d0ec2b6c766e01365a7
Bruno Franco <brunofvilela1996@gmail.com>
2022-01-18 10:31:30 -0300

    gitignore

--- node-user-authentication-api/src/db.ts
+++ node-user-authentication-api/src/db.ts
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
 
-const connectionString = '';
+const connectionString = 'postgres://ndtjomvp:2DxCe7iGnPsxg6dTTGg79O8q2xb9aBlB@motty.db.elephantsql.com/ndtjomvp';
 const db = new Pool({connectionString})
 export default db;
